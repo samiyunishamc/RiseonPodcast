57f152f18ecd712858d2bf2d795b79cc7c3a1c5a
sathish <sathishyadav8898@gmail.com>
2026-04-12 12:26:15 +0530

    Add admin login button to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -72,6 +72,9 @@ const Navbar = () => {
               {link.label}
             </button>
           ))}
+          <a href="/admin" className="btn-outline !py-2 !px-4 text-sm no-underline">
+            Admin Login
+          </a>
         </nav>
 
         <button
@@ -104,6 +107,13 @@ const Navbar = () => {
                   {link.label}
                 </button>
               ))}
+              <a
+                href="/admin"
+                className="btn-outline text-center no-underline mt-1"
+                onClick={() => setMenuOpen(false)}
+              >
+                Admin Login
+              </a>
             </div>
           </motion.nav>
         )}

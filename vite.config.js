ed0c129a602a7a4c5dc81ec38a2b0dc316c778b8
Samiyunisha <samiyunishamc@gmail.com>
2026-06-20 11:19:29 +0530

    Fix Vite config and merge conflict

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -11,17 +11,6 @@ export default defineConfig({
     historyApiFallback: true,
   },
 })
-=======
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    // Serve index.html for all routes (SPA behaviour)
-    historyApiFallback: true,
-  },
-})
->>>>>>> a3e83a631e6aa3d66034697253f1a72e544ca08e
+
+
